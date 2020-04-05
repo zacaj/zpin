4161877bfa5412e22215bdb57ae1e37c88e9a383
zacaj <zacaj@zacaj.com>
2020-04-05 19:30:54 -0400

    prevent decimal board data

--- boards.ts
+++ boards.ts
@@ -36,7 +36,7 @@ export class Solenoid16 {
     }
 
     fireSolenoidFor(num: number, onTime: number) {
-        return this.send(`f ${num} ${onTime}`);
+        return this.send(`f ${num} ${onTime.toFixed()}`);
     }
 
     disableSolenoid(num: number) {
@@ -45,12 +45,12 @@ export class Solenoid16 {
 
     initMomentary(num: number, onTime = 50) {
         Log.info(['mpu', 'solenoid'], 'init momentary %i on board %i', num, this.board);
-        return this.send(`is m ${num} ${onTime}`);
+        return this.send(`is m ${num} ${onTime.toFixed()}`);
     }
 
     initOnOff(num: number, maxOnTime = 0, pulseOffTime = 0) {
         Log.info(['mpu', 'solenoid'], 'init on-off %i on board %i', num, this.board);
-        return this.send(`is oo ${num} ${maxOnTime} ${pulseOffTime}`);
+        return this.send(`is oo ${num} ${maxOnTime.toFixed()} ${pulseOffTime.toFixed()}`);
     }
 
     initInput(num: number, settleTime = 3) {
