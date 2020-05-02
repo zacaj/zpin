b94d7419cf5741c42f28b61df3ea77da8383630b
zacaj <zacaj@zacaj.com>
2020-05-01 20:02:04 -0400

    improve syncr

--- syncr.ts
+++ syncr.ts
@@ -5,10 +5,11 @@ const scp = require('scp2').Client;
 // if (!process.argv[2]) {
 //     throw 'remote path required';
 // }
-const remotePath = 'zpin';
+const remotePath = process.argv[3] || 'zpin';
+const localPath = process.argv[4] || './';
 const address = process.argv[2] || '192.168.2.45';
-const username = 'pi' || process.argv[4];
-const password = 'pass' || process.argv[5];
+const username = 'pi' || process.argv[5];
+const password = 'pass' || process.argv[6];
 const toCopy: string[] = [];
 const client = new scp({
     port: 22,
@@ -20,13 +21,13 @@ const client = new scp({
 /*console.log("starting...");
 copyFile('./**').then(() =>*/ {
     console.log('watching, waiting');
-    fs.watch('./', {
+    fs.watch(localPath, {
         recursive: true,
     }, async (eventType, filename) => {
         if (filename?.startsWith('.')) {
  /*console.log('skip dot');*/ return;
         }
-        if (filename && fs.statSync(path.resolve('./', filename)).isDirectory()) {
+        if (filename && fs.statSync(path.resolve(localPath, filename)).isDirectory()) {
             // console.log('skip dir');
             return;
         }
@@ -62,7 +63,8 @@ function copyFile(filename: string): Promise<void> {
         //     if (err) reject(err);
         //     else resolve();
         // });
-        client.upload(filename, remote, (err: Error) => {
+        console.log('copy %s to %s', path.join(localPath, filename), remote);
+        client.upload(path.join(localPath, filename), remote, (err: Error) => {
             if (err) reject(err);
             else resolve();
         });
