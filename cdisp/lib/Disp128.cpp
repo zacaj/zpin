87411164f27747f63a8293f8c61343f200dbaf7e
zacaj <zacaj@zacaj.com>
2020-11-06 10:49:00 -0500

    fix disp 128

--- cdisp/lib/Disp128.cpp
+++ cdisp/lib/Disp128.cpp
@@ -1,7 +1,8 @@
 #include "Disp128.h"
-
+#include <stdio.h>
 
 Disp128::Disp128(int number, MIRROR_IMAGE mirror, ROTATE_IMAGE rotate): 
-    ST7735(number, 128, 128, mirror, rotate) {
-
+    ST7735(number, 128, 128, L2R_U2D, mirror, rotate) {
+    xOffset = 2;
+    yOffset = 1;
 }
