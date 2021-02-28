b276a01087703810442a40b4670ee8457dba43ea
zacaj <zacaj@zacaj.com>
2021-02-27 22:26:46 -0500

    attract mode

--- attract.ts
+++ attract.ts
@@ -2,9 +2,11 @@ import { argv } from 'yargs';
 import { Game } from './game';
 import { addToScreen, makeText, ModeGroup, Screen } from './gfx';
 import { initMachine } from './init';
+import { Color } from './light';
 import { Log } from './log';
 import { machine } from './machine';
 import { Mode, Modes } from './mode';
+import { Outputs } from './outputs';
 import { fork } from './promises';
 import { onSwitchClose } from './switch-matrix';
 import { money, score } from './util';
@@ -22,6 +24,37 @@ export class AttractMode extends Mode {
         public scores?: [number, number][],
     ) {
         super(Modes.AttractMode);
+
+        this.out = new Outputs(this, {
+            lPower1: [[Color.White, 'fl', 1, 1]],
+            lPower2: [[Color.White, 'fl', 1, 0]],
+            lPower3: [[Color.White, 'fl', 1, 1]],
+            lLaneLower1: [[Color.Yellow, 'fl', 1, 1]],
+            lLaneLower2: [[Color.Yellow, 'fl', 1, 0]],
+            lLaneLower3: [[Color.Yellow, 'fl', 1, 1]],
+            lLaneLower4: [[Color.Yellow, 'fl', 1, 0]],
+            lMagnet1: [[Color.Green, 'fl', 1, 1]],
+            lMagnet2: [[Color.Green, 'fl', 1, 0]],
+            lMagnet3: [[Color.Green, 'fl', 1, 1]],
+            lShootAgain: [[Color.Purple, 'fl', 1, 1]],
+            lPopperStatus: [[Color.Green, 'fl', 1, 0]],
+            lRampArrow: [[Color.Blue, 'fl', 1, 0]],
+            lMainTargetArrow: [[Color.Pink, 'fl', 1, 1]],
+            lRampMini: [[Color.Orange, 'fl', 1, 1]],
+            lLeftArrow: [[Color.Orange, 'fl', 1, 0]],
+            lSideShotArrow: [[Color.White, 'fl', 1, 1]],
+            lSideTargetArrow: [[Color.Red, 'fl', 1, 0]],
+            lEjectArrow: [[Color.Yellow, 'fl', 1, 0]],
+            lUpperLaneArrow: [[Color.Red, 'fl', 1, 1]],
+            lUpperLaneTarget: [[Color.Orange, 'fl', 1, 0]],
+            lUpperTargetArrow: [[Color.White, 'fl', 1, 1]],
+            lSpinnerArrow: [[Color.Blue, 'fl', 1, 1]],
+            lSpinnerTarget: [[Color.Orange, 'fl', 1, 0]],
+            lLaneUpper1: [[Color.Orange, 'fl', 1, 0]],
+            lLaneUpper2: [[Color.Orange, 'fl', 1, 1]],
+            lLaneUpper3: [[Color.Orange, 'fl', 1, 0]],
+            lLaneUpper4: [[Color.Orange, 'fl', 1, 1]],                        
+        });
 
         addToScreen(() => new AttractGfx(this));
 
