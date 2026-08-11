97f347068ca98536f1cfe4d2ab20c60e8827172c
zacaj <zacaj@zacaj.com>
2026-08-10 21:36:32 -0400

    Tune display and projector calibration offsets

--- cdisp/lib/Disp128.cpp
+++ cdisp/lib/Disp128.cpp
@@ -4,5 +4,5 @@
 Disp128::Disp128(int number, ROTATE_IMAGE rotate, MIRROR_IMAGE mirror): 
     ST7735(number, 128, 128, rotate==ROTATE_0 || rotate==ROTATE_90? L2R_U2D : R2L_D2U, 1, rotate, mirror) {
     xOffset = 2;
-    yOffset = 1;
+    yOffset = 3;
 }
